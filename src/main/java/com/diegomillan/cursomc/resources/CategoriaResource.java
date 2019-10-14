25624416c8b64601ad8565f11962199237241bd3
Diego Millan <die9o.r@gmail.com>
2019-10-14 14:14:42 -0300

    added h2 database

--- src/main/java/com/diegomillan/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/diegomillan/cursomc/resources/CategoriaResource.java
@@ -15,6 +15,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
+		
 		Categoria cat1 = new Categoria(1,"Informatica");
 		Categoria cat2 = new Categoria(2,"Escritório");
 		
