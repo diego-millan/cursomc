b29dad8653f7461cd5fa2fb223b45271d5697b20
Diego Millan <die9o.r@gmail.com>
2019-10-14 13:46:00 -0300

    added first domain

--- src/main/java/com/diegomillan/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/diegomillan/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.diegomillan.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.diegomillan.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Resting Successfully";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		
+		return categorias;
 	}
 }
