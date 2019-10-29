a3f05778b785e4f20887900665026de7ae41da82
Diego Rodrigo Millan <30292584+diego-millan@users.noreply.github.com>
2019-10-29 17:54:43 -0300

    [master] - added existing email validation

--- src/main/java/com/diegomillan/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/diegomillan/cursomc/repositories/ClienteRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.diegomillan.cursomc.domain.Cliente;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+    @Transactional
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/diegomillan/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/diegomillan/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.diegomillan.cursomc.services.validation;
 
+import com.diegomillan.cursomc.domain.Cliente;
 import com.diegomillan.cursomc.domain.enums.TipoCliente;
 import com.diegomillan.cursomc.dto.ClienteInsertionDTO;
+import com.diegomillan.cursomc.repositories.ClienteRepository;
 import com.diegomillan.cursomc.resources.exception.FieldErrorMessage;
 import com.diegomillan.cursomc.services.validation.utils.DocumentUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteInsertionDTO> {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
     public void Initialize(ClienteInsert ann) {
 
     }
@@ -32,6 +39,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
             if(!DocumentUtil.isValidCNPJ(clienteDTO.getCpfOuCnpj())) {
                 errorMessageList.add(new FieldErrorMessage("cpfOuCnpj", "CNPJ Inválido"));
             }
+        }
+
+        Cliente clienteComEmailExistente = clienteRepository.findByEmail(clienteDTO.getEmail());
+        if(clienteComEmailExistente != null) {
+            errorMessageList.add(new FieldErrorMessage("email", "E-mail já cadastrado no sistema"));
         }
 
         for(FieldErrorMessage e : errorMessageList) {
